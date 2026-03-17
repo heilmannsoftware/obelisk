3a7ae617523fac900fb42fa3c3e6dbc72a731db2
Johannes Witzig <jwitzig@heilmannsoftware.de>
2026-03-17 17:24:19 +0100

    Parametrize compressedWasm over the name of the frontend

--- default.nix
+++ default.nix
@@ -104,11 +104,12 @@ in rec {
     };
   compressedWasm = frontend: optimizationLevel: externs: pkgs.runCommand "compressedWasm" { } ''
     set -euo pipefail
-    mkdir -p $out/frontend.jsexe
-    ln -s ${pkgs.runCommand "jsffi.js" { buildInputs = [pkgs.nodejs]; } "$(${frontend.compiler}/bin/wasm32-wasi-ghc --print-libdir)/post-link.mjs -i ${frontend}/bin/frontend.wasm -o $out"} $out/frontend.jsexe/ghc_wasm_jsffi.js
-    ln -s ${pkgs.runCommand "frontend.optimized.wasm" {} "${pkgs.binaryen}/bin/wasm-opt --low-memory-unused --strip-dwarf --converge -ol 2 -s 1 ${frontend}/bin/frontend.wasm -o $out"} $out/frontend.jsexe/frontend.wasm
-    cp ${./wasm-shim.js} $out/frontend.jsexe/all.js
-    cp ${wasi-shim}  $out/frontend.jsexe/wasi-shim.js
+    outdir="$out/${frontend.pname}.jsexe"
+    mkdir -p $outdir
+    ln -s ${pkgs.runCommand "jsffi.js" { buildInputs = [pkgs.nodejs]; } "$(${frontend.compiler}/bin/wasm32-wasi-ghc --print-libdir)/post-link.mjs -i ${frontend}/bin/${frontend.pname}.wasm -o $out"} $outdir/ghc_wasm_jsffi.js
+    ln -s ${pkgs.runCommand "${frontend.pname}.optimized.wasm" {} "${pkgs.binaryen}/bin/wasm-opt --low-memory-unused --strip-dwarf --converge -ol 2 -s 1 ${frontend}/bin/${frontend.pname}.wasm -o $out"} $outdir/${frontend.pname}.wasm
+    substitute ${./wasm-shim.js} $outdir/all.js --replace-fail '@@FRONTEND_NAME@@' '${frontend.pname}'
+    cp ${wasi-shim} $outdir/wasi-shim.js
   '';
 
   serverModules = {
--- wasm-shim.js
+++ wasm-shim.js
@@ -6,16 +6,18 @@ import {
 } from "./wasi-shim.js";
 import ghc_wasm_jsffi from "./ghc_wasm_jsffi.js";
 
+const frontend_wasm = "@@FRONTEND_NAME@@.wasm";
+
 // -T is for RTS stats, -c to use compacting gc for the larges generation in an attempt to not grow max use too much
-const args = ["frontend.wasm", "+RTS", "-H64m", "-c", "-T", "-RTS"];
+const args = [frontend_wasm, "+RTS", "-H64m", "-c", "-T", "-RTS"];
 const env = [];
 const fds = [
   new OpenFile(new File(new Uint8Array(), { readonly: true })),
   ConsoleStdout.lineBuffered((msg) =>
-    console.info(`[frontend.wasm] ${msg}`)
+    console.info(`[${frontend_wasm}] ${msg}`)
   ),
   ConsoleStdout.lineBuffered((msg) =>
-    console.error(`[frontend.wasm] ${msg}`)
+    console.error(`[${frontend_wasm}] ${msg}`)
   ),
 ];
 const options = { debug: false };
@@ -23,7 +25,7 @@ const wasi = new WASI(args, env, fds, options);
 
 const instance_exports = {};
 const { instance } = await WebAssembly.instantiateStreaming(
-  fetch("./ghcjs/frontend.wasm"),
+  fetch("./ghcjs/" + frontend_wasm),
   {
     wasi_snapshot_preview1: wasi.wasiImport,
     ghc_wasm_jsffi: ghc_wasm_jsffi(instance_exports),
